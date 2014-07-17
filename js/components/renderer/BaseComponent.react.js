65a50fcf592ad4c36d66f18d517356bc92db333f
Cedric Dugas <cedric.dugas@ssense.com>
2014-07-17 15:05:51 -0400

    moved todo js

---
+++ js/components/renderer/BaseComponent.react.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2013-2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+var RendererActions = require('../actions/RendererActions');
+
+var BaseComponent = React.createClass({
+
+  /**
+   * @return {object}
+   */
+  render: function() {
+    return (
+      <div class="visual_component top5 left2 width10 height2"></div>
+    );
+  },
+
+  /**
+   * Event handler called within TodoTextInput.
+   * Defining this here allows TodoTextInput to be used in multiple places
+   * in different ways.
+   * @param {string} text
+   */
+  _onSave: function(text) {
+    TodoActions.create(text);
+  }
+
+});
+
+module.exports = Header;
